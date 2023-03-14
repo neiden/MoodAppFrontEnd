e42437523cfac9583ef04e78bb579818ee33ef87
Gibsonchan98 <tiger61898@gmail.com>
2023-03-14 13:04:05 -0700

    added components

--- moodweb/src/app/app.module.ts
+++ moodweb/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
