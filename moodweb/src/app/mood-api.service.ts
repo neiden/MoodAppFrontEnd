8ead55174646f5bb03e1040c7a611b69a37beebc
Gibsonchan98 <tiger61898@gmail.com>
2023-03-16 16:29:17 -0700

    http workings

--- moodweb/src/app/mood-api.service.ts
+++ moodweb/src/app/mood-api.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AccountServiceService {
+export class MoodAPIService {
+
+  //apiRoot : string "";
 
   constructor() { }
 }
--- moodweb/src/app/page/page.component.ts
+++ moodweb/src/app/page/page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page',
@@ -7,10 +8,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./page.component.css']
 })
 export class PageComponent {
-
-  constructor( private activatedRoute: ActivatedRoute) {} 
+  //need to get http request and change file names and such
+  username : string = "";
+  password : string = "";
+  constructor(private router:Router, private activatedRoute: ActivatedRoute) {} 
 
   autheUser(){
     console.log('authenticate user')
+  }
+
+  goToRegister(){
+    this.router.navigateByUrl('/register');
   }
 }
