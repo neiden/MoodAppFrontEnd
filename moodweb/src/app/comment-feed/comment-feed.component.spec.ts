a34ac47e90082c66d1dda1dbce1cbd192fa05488
Divyarajsinh Rana <divyarajsinhrana@Divyarajsinhs-MacBook-Air.local>
2023-03-17 21:49:14 -0500

    adding back comment component

---
+++ moodweb/src/app/comment-feed/comment-feed.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { CommentFeedComponent } from './comment-feed.component';
+
+// describe('CommentFeedComponent', () => {
+//   let component: CommentFeedComponent;
+//   let fixture: ComponentFixture<CommentFeedComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ CommentFeedComponent ]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(CommentFeedComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
