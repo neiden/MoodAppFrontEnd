95b679df1e92d0b87fc1bd62928ac4ed688bb1d2
neiden <pinkgodzela@gmail.com>
2023-03-20 18:42:44 -0700

    Added playlist creation and returns playlist info + link

--- moodweb/src/app/spotify-playlist-view/spotify-playlist-view.component.ts
+++ moodweb/src/app/spotify-playlist-view/spotify-playlist-view.component.ts
@@ -14,6 +14,10 @@ export class SpotifyPlaylistViewComponent {
 
   mood: FormControl = new FormControl('');
   token: string = '';
+  songArr: string[] = [];  
+  songNames: string[] = [];
+  songArtists: string[] = [];
+  spotifyLink: string = '';
   
 
   authenticate(){
@@ -23,8 +27,28 @@ export class SpotifyPlaylistViewComponent {
 
 
   createPlaylist(){
+    
     this.token = this.service.getToken();
-    this.service.createPlaylist(this.mood.value, this.token);
+    this.service.getTracks(this.mood.value, this.token).subscribe((data: any) =>{
+      console.log(data);
+      const arr = data['tracks']['items'];
+      for (var i = 0; i < arr.length; i++){
+        console.log("Name of track: " + arr[i]['name'] + " uri of track: " + arr[i]['uri'])
+        this.songArr.push(arr[i]['uri']);
+        this.songNames.push(arr[i]['name']);
+        this.songArtists.push(arr[i]['artists'][0]['name']);
+      }
+      this.service.getUserId(this.token).subscribe((data2: any) => {
+        this.service.createPlaylist(this.songArr, this.token, data2["id"]).subscribe((data3: any) => {
+          this.spotifyLink = data3['external_urls']['spotify'];
+          this.service.populatePlaylist(this.songArr, this.token, data3['id']).subscribe((data: any) => {
+            console.log("Songs: " + this.songNames + " Song artists: " + this.songArtists + "Spotify link: " + this.spotifyLink);
+          })
+        })
+      })
+      //this.service.createPlaylist(this.songArr, this.token);
+    });
   }
+
 
 }
