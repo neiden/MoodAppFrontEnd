7843630e49aa185402dbd323fa09e58cb3123e4e
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-03-20 23:56:03 -0700

    register writes to db

--- moodweb/src/app/mood-api.service.ts
+++ moodweb/src/app/mood-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, firstValueFrom } from 'rxjs';
+import { Account } from 'src/models/account';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +16,18 @@ export class MoodAPIService {
 
   constructor(private http: HttpClient) { }
 
-  loginUser( arr : string[])  {
+  loginUser(arr : string[])  {
     let geturl = "https://moodapiv2.azurewebsites.net/User/Login?info=" + arr[0] + "&info=" + arr[1];
     return this.http.get(geturl) as Observable<Array<string>>;
   }
 
-  registerUser(value:any){
-    return this.http.post(this.apiRoot, "Sarah");
+  registerUser(acc : Account){
+    acc.username = "VelmaJinkies1";
+    acc.zipcode = "123456";
+    console.log(acc);
+    //https://moodapiv2.azurewebsites.net/User/Users?info=Daphne&info=Blake&info=DaccoutBlake1&info=jeepers%40scooby.com&info=369852%21S&info=54652
+    let posturl = "https://moodapiv2.azurewebsites.net/User/Users?info=" + acc.firstname + "&info=" + acc.lastname + "&info=" + acc.username + "&info=" + acc.email + "&info=" + acc.password + "&info=" + acc.zipcode;
+    return this.http.post(posturl, acc);
   }
 
   getAllPosts() : Observable<string> {
