eeaef44143a9fe3cbcebfd64df5f3222be85469e
neiden <pinkgodzela@gmail.com>
2023-03-23 23:58:56 -0700

    fixed routing

--- moodweb/src/app/app-routing.module.ts
+++ moodweb/src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ import { CommentComponent } from './comment/comment.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: LoginComponent
+  },
+  {
     path: 'login', 
     component: LoginComponent
   },
